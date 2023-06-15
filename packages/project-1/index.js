d20d840b70d903bbb33d75d18174017af45346cb
adarshTS <adarsh.ts8@gmail.com>
2023-06-15 18:13:21 +0530

    SHA

--- packages/project-1/index.js
+++ packages/project-1/index.js
@@ -17,7 +17,7 @@ export async function RunTest(){
     await driver.get('https://www.browserstack.com/pricing');
     await percySnapshot(driver, 'Pricing');
 
-    await driver.get('https://www.browserstack.com/users/sign_up');
+    await driver.get('https://www.browserstack.com/users/sign_in');
     await percySnapshot(driver, 'Sign up Page');
 
   } finally {
