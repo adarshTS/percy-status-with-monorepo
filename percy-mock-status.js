e7f06204598fa92f956436f3ec8e7325a344b0ad
adarshTS <adarsh.ts8@gmail.com>
2023-06-15 19:27:32 +0530

    updated proj1

--- percy-mock-status.js
+++ percy-mock-status.js
@@ -1,56 +1,48 @@
-import { exec } from 'child_process';
-import axios from 'axios';
-import dotenv from 'dotenv';
-import fetch from 'node-fetch';
+import {exec} from 'child_process'
+import axios from 'axios'
+import dotenv from 'dotenv'
+import Fetch from 'node-fetch'
 
-global.fetch = fetch;
-dotenv.config();
+global.fetch = Fetch
+dotenv.config()
 
-const GITHUB_TOKEN = process.env.SECRET_TOKEN;
-const REPO = process.env.REPO;
-const SHA = process.env.SHA;
-
-async function GetAffected() {
-  return new Promise((resolve, reject) => {
-    exec('npx nx print-affected', (err, result) => {
-      if (err) reject(err);
-      resolve(JSON.parse(result));
-    });
-  });
+const GITHUB_TOKEN = process.env.SECRET_TOKEN
+const REPO = process.env.REPO
+const SHA = process.env.SHA
+async function GetAffected(){
+    return new Promise((resolve,reject)=>{
+        exec('npx nx print-affected',(err,result)=>{
+            if(err) reject(err)
+            resolve(JSON.parse(result))
+        })
+    })
 }
 
-async function MarkStatus(project_slug) {
-  let endpoint = `https://api.github.com/repos/${REPO}/statuses/${SHA}`;
-  console.log(endpoint);
-  try {
-    const response = await fetch(endpoint, {
-      method: 'POST',
-      headers: {
-        Accept: 'application/vnd.github+json',
-        Authorization: `Bearer ${GITHUB_TOKEN}`,
-        'X-GitHub-Api-Version': '2022-11-28',
-      },
-      body: JSON.stringify({
-        state: 'success',
-        context: `percy/${project_slug}`,
-      }),
-    });
-
-    console.log(await response.text());
-  } catch (error) {
-    console.error(error);
-  }
+async function MarkStatus(project_slug){
+    let endpoint = `https://api.github.com/repos/${REPO}/statuses/${SHA})`
+    console.log(endpoint)
+    return fetch(endpoint,{
+        headers:{
+            Accept:"application/vnd.github+json",
+            Authorization:`Bearer ${GITHUB_TOKEN}`,
+            "X-GitHub-Api-Version":"2022-11-28"
+        },
+        body:JSON.stringify({
+            state:'success',
+            context:`percy/${project_slug}`
+        })
+    }).then(async (res)=>{
+        console.log(await res.text())
+    })
 }
 
-(async () => {
-  let affected = await GetAffected();
-  let affectedProjects = affected.projects;
-  let allProjects = affected.projectGraph.nodes;
-  let unaffectedProjects = allProjects.filter(
-    (p) => !affectedProjects.some((q) => p == q)
-  );
+(async ()=>{
+    let affected = await GetAffected()
+    let affectedProjects = affected.projects
+    let allProjects = affected.projectGraph.nodes
+    let unaffectedProjects = allProjects.filter((p)=>!affectedProjects.some((q)=>p ==q));
 
-  for (let project of unaffectedProjects) {
-    await MarkStatus(project);
-  }
-})();
+    unaffectedProjects.forEach((project)=>{
+        MarkStatus(project)
+    })
+})()
